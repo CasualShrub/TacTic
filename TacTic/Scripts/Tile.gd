37195ca276c5025c5ba94bb653cb7f36dafbae68
Casual Shrub <alexfrancia745@gmail.com>
2023-02-21 12:01:06 +0800

    Tiles now have a Vector2 to represent position in grid

--- TacTic/Scripts/Tile.gd
+++ TacTic/Scripts/Tile.gd
@@ -1,34 +1,38 @@
 extends Area2D
 
-var variant = 0
+var tile_type = 0
+var tile_id = Vector2(0,0)
+var shape_type = "empty"
+
 var has_object = false
 var is_hovered = false
 
 signal shape_added(shape)
 
 func set_variant(value):
-	variant = value
-	$BackgroundSprite.play("variant%d" % variant)
+	tile_type = value
+	$BackgroundSprite.play("variant%d" % tile_type)
 
 func set_shape(value):
 	has_object = true;
-	$ObjectSprite.play("%s" % value)
-	emit_signal("shape_added", value)
+	shape_type = value
+	$ObjectSprite.play("%s" % shape_type)
+	emit_signal("shape_added", shape_type)
 
 func _on_Tile_input_event(_viewport, event, _shape_idx):
 	if is_hovered:
 		if (event.is_pressed()):
-			$BackgroundSprite.play("variant%d_clicked" % variant)
+			$BackgroundSprite.play("variant%d_clicked" % tile_type)
 		elif (event is InputEventMouseButton && event.is_action_released("click")):
-			$BackgroundSprite.play("variant%d" % variant)
+			$BackgroundSprite.play("variant%d" % tile_type)
 			set_shape("O")
 
 func _on_Tile_mouse_entered():
 	is_hovered = true
 	if !has_object:
-		$BackgroundSprite.play("variant%d_hovered" % variant)
+		$BackgroundSprite.play("variant%d_hovered" % tile_type)
 
 func _on_Tile_mouse_exited():
 	is_hovered = false
 	if !has_object:
-		$BackgroundSprite.play("variant%d" % variant)
+		$BackgroundSprite.play("variant%d" % tile_type)
--- TacTic/Scripts/TileGrid.gd
+++ TacTic/Scripts/TileGrid.gd
@@ -10,6 +10,7 @@ func _ready():
 	
 	tile_count = tile_array.size()
 	design_tiles()
+	setup_tile_vectors()
 			
 			
 	pass # Replace with function body.
@@ -26,3 +27,12 @@ func design_game_start():
 	tile_array[7].set_shape("X")
 	tile_array[8].set_shape("O")
 
+func evaluate_grid():
+	pass
+		
+func setup_tile_vectors():
+	for i in tile_array.size():
+		var x = i % 3
+		var y = i / 3
+		tile_array[i].tile_id = Vector2(x, y)
+		print("Tile %d has coordinates (%d, %d)" % [i,x,y])
