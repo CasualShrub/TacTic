820cafe3bd508701325d2b586461aa19dfc76335
Alex Francia <alex.francia@kooapps.com>
2023-03-06 11:34:31 +0800

    Converted project to Godot 4.0 Stable
    Decided to swap over to Godot 4 since it was just released. May be buggy, but I'd say it's worth exploring since this is my first Godot project anyway.

--- TacTic/Scripts/DefaultScene.gd
+++ TacTic/Scripts/DefaultScene.gd
@@ -6,7 +6,7 @@ func _ready():
 	
 	for i in $TileGrid.tile_count :
 		var tile = get_node("TileGrid/Tile%d" % (i+1) )
-		tile.connect("shape_added", self, "on_shape_added")
+		tile.connect("shape_added",Callable(self,"on_shape_added"))
 
 func on_shape_added(shape):
 	print("ADDED SHAPE " + shape)
