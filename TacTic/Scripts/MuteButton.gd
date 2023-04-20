747fb01dc853b3ff42816b29769d7b41b19bdb51
CasualShrub <120472172+CasualShrub@users.noreply.github.com>
2023-04-20 12:11:13 +0800

    Added mute button to settings menu

---
+++ TacTic/Scripts/MuteButton.gd
@@ -0,0 +1,21 @@
+extends "res://Scripts/ClickableObject.gd"
+
+var is_muted: bool
+
+func _ready():
+	super()
+	is_muted = false
+	
+#TODO: change to resource/nodetype
+func on_input_event(_viewport, event, _shape_idx):
+	if (event.is_pressed()):
+		on_pressed()
+	elif (event.is_action_released("click") && GameManager.is_pressing):
+		on_released();
+		
+func on_pressed():
+	BackgroundSprite.play("click")
+	GameManager.is_pressing = true
+func on_released():
+	BackgroundSprite.play("idle")
+	GameManager.is_pressing = false
