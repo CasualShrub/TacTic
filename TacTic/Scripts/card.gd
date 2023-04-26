47dfddcd49882cc1999a71eb010d99e81dc2fff1
CasualShrub <120472172+CasualShrub@users.noreply.github.com>
2023-04-26 20:56:26 +0800

    Fix parser error cached filename (2)

---
+++ TacTic/Scripts/card.gd
@@ -0,0 +1,77 @@
+class_name Card
+extends Node2D
+
+var shape_type: String = "empty"
+var is_hovered: bool = false
+var is_gone: bool = false
+var position_in_hand: int = 0
+
+var mpos: Vector2 = Vector2()
+
+@onready var BackgroundSprite = $BackgroundSprite
+@onready var ShapeSprite = $ShapeSprite
+
+signal card_dragged
+
+func _ready():
+	randomize_shape()
+	EventManager.card_drag_fail.connect(Callable(self,"on_card_drag_fail"))
+	EventManager.card_drag_success.connect(Callable(self,"on_card_drag_success"))
+	
+func _input(event):
+	if event.is_action_pressed("randomize"):
+		randomize_shape()
+	elif event.is_action_pressed("draw"):
+		draw()
+
+func draw():
+	pass
+
+func randomize_shape():
+	var random_int = randi_range(0,2)
+	match random_int:
+		0:
+			shape_type = "O"
+		1:
+			shape_type = "X"
+		2:
+			shape_type = "Triangle"
+	
+	ShapeSprite.play(shape_type)
+
+func _on_mouse_entered():
+	if !is_gone:
+		is_hovered = true
+		BackgroundSprite.play("%s_hovered" % "cardback")
+
+func _on_mouse_exited():
+	if !is_gone:
+		is_hovered = false
+		BackgroundSprite.play("%s_default" % "cardback")
+
+func on_card_drag_fail():
+	if is_gone:
+		reappear()
+
+func on_card_drag_success():
+	if is_gone:
+		EventManager.emit_card_removed_from_hand(position_in_hand)
+		self.queue_free()
+
+func _on_input_event(_viewport, event, _shape_idx):
+	if is_hovered:
+		if (event.is_pressed()):
+			disappear()
+			GameManager.is_dragging = true
+			ShapeSprite.play("empty")
+			ShapeSprite.visible = false
+			emit_signal("card_dragged", shape_type)
+
+func disappear():
+	BackgroundSprite.play("%s_dissapear" % "cardback")
+	is_gone = true
+
+func reappear():
+	BackgroundSprite.play_backwards("%s_dissapear" % "cardback")
+	ShapeSprite.visible = true #TODO: call this at the end of the animation?
+	is_gone = false
---
+++ TacTic/Scripts/tile.gd
@@ -0,0 +1,72 @@
+class_name Tile
+extends Area2D
+
+var tile_type = 0
+var tile_id = Vector2(0,0)
+var shape_type = "empty"
+
+var shape_being_dragged = "empty"
+
+var has_object = false
+var is_hovered = false
+
+var adjacent_tiles: Array[Tile]
+
+@onready var BackgroundSprite = $BackgroundSprite
+@onready var ObjectSprite = $ObjectSprite
+
+signal shape_added(shape)
+
+signal card_dropped(is_valid_drop)
+
+func set_variant(value):
+	tile_type = value
+	BackgroundSprite.play("variant%d" % tile_type)
+
+func modulate_color(shade):
+	BackgroundSprite.modulate = shade
+
+func set_shape(value):
+	has_object = true;
+	shape_type = value
+	
+	if (shape_being_dragged != "empty"):
+		shape_being_dragged = "empty"
+	
+	ObjectSprite.play("%s" % shape_type)
+	emit_signal("shape_added", shape_type)
+
+func highlight_shape():
+	if (shape_type != "empty"):
+		ObjectSprite.play("%s_win" % shape_type)
+
+func _on_Tile_input_event(_viewport, event, _shape_idx):
+	if is_hovered:
+		if (event.is_pressed()):
+			BackgroundSprite.play("variant%d_clicked" % tile_type)
+			show_adjacents()
+		elif (event.is_action_released("click") && GameManager.is_dragging):
+			if (!has_object):
+				BackgroundSprite.play("variant%d" % tile_type)
+				set_shape(GameManager.shape_being_dragged)
+				GameManager.is_dragging = false
+				EventManager.emit_card_drag_success()
+			else:
+				#emit signal to return shape to original container
+				pass
+
+func show_adjacents():
+	print("Tile (%d,%d) has adjacent tiles: " % [tile_id.x, tile_id.y])
+	for tile in adjacent_tiles:
+		print("Tile (%d,%d)" % [tile.tile_id.x, tile.tile_id.y])
+		tile.modulate_color(Color("YELLOW"))
+
+func _on_Tile_mouse_entered():
+	is_hovered = true
+	if !has_object:
+		BackgroundSprite.play("variant%d_hovered" % tile_type)
+
+func _on_Tile_mouse_exited():
+	is_hovered = false
+	if !has_object:
+		BackgroundSprite.play("variant%d" % tile_type)
