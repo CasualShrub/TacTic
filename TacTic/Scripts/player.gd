3ba2494842b694d2818fa1aa998a8210dc11cdd1
CasualShrub <120472172+CasualShrub@users.noreply.github.com>
2023-04-28 03:50:33 +0800

    Added shake tween when player takes damage

--- TacTic/Scripts/player.gd
+++ TacTic/Scripts/player.gd
@@ -10,3 +10,9 @@ func _ready():
 func on_player_damaged():
 	Animator.play("hurt")
 	Animator.queue("idle")
+	
+	#TODO: Consider refactoring to use animator instead of tween?
+	var tween: Tween = create_tween()
+	tween.tween_property(self, "position", (self.position + Vector2(1,0)), 0.2).set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_property(self, "position", (self.position + Vector2(-1,0)), 0.2).set_trans(Tween.TRANS_ELASTIC)
+	tween.parallel().tween_property(self, "position", (self.position + Vector2(0,0)), 0.2).set_trans(Tween.TRANS_ELASTIC)
