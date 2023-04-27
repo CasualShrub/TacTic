6ee44618cabdfe7c34e441383314d58138abc547
CasualShrub <120472172+CasualShrub@users.noreply.github.com>
2023-04-27 11:56:09 +0800

    Improved panel area hitbox node structure
    This also includes invalid area hitbox improvements.

--- TacTic/Scripts/invalid_area.gd
+++ TacTic/Scripts/invalid_area.gd
@@ -2,6 +2,6 @@ extends Area2D
 
 signal drag_failed #TODO: Edit tiles so that they also emit drag_failed OR dispatch
 
-func _on_input_event(viewport, event, shape_idx):
+func _on_input_event(_viewport, event, _shape_idx):
 	if (event.is_action_released("click") && GameManager.is_dragging):
 		EventManager.emit_card_drag_fail()
